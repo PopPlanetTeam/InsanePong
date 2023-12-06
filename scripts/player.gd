bccd572a279d182b1596cddda1eeefeb751d6cdd
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-12-06 18:41:30 -0300

    Finishing tutorial

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 const SPEED = 300.0;
 @export var side = 'p1';
 
-var direction = Vector2.ZERO;
-
 func _physics_process(delta):
+	var direction
+	
 	if side == 'p1':
 		direction = get_axis(KEY_W, KEY_S);
 	else:
@@ -21,9 +21,9 @@ func _physics_process(delta):
 func get_axis(up, down):
 	if Input.is_key_pressed(up):
 		return -1;
-	if Input.is_key_pressed(down):
+	elif Input.is_key_pressed(down):
 		return 1;
 
 func _on_area_2d_body_entered(body):
-	#body.direction.x *= -1;
-	body.direction.y *= -1;
+	body.direction.x *= -1
+	Main.side = side;
